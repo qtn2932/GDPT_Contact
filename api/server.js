98ba39a20ca75a1415d7aaef43ab9b9416c6cfc8
unknown <quan.nguyen199712@gmail.com>
2018-12-11 14:10:53 -0500

    added cors for cross domain data access

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express= require('express');
 const server= express();
+const cors= require('cors');
 server.use(express.json());
+server.use(cors());
 
 const ht= require('../routes/huynhtruong.js');
 const thieuNam= require('../routes/thieunam.js')
