22cb348b3a4a6cb46181dfd500af34364ce4272b
unknown <quan.nguyen199712@gmail.com>
2018-12-10 21:42:15 -0500

    update knex file with the correct database, added data base

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server= require('./api/server.js');
-const port= process.env.PORT || 3300;
+const port= process.env.PORT || 3300; //port is dynamic for heroku deploy, change as neccessary for other deployment method
 server.listen(port, ()=>{
     console.log('Up and running @ 3300 or whatever')
 })
