c53b464b07da2b9077f575e0e40f03114d0b5302
unknown <quan.nguyen199712@gmail.com>
2018-12-11 00:13:50 -0500

    get specific ht info function added

--- api/server.js
+++ api/server.js
@@ -3,11 +3,13 @@ const server= express();
 server.use(express.json());
 
 const ht= require('../routes/huynhtruong.js');
+const db=require('../data/db.js');
 
 //insanity check
 server.get('/', (req,res)=>{
     res.json({message:'server is ready'})
 })
+
 
 server.use('/huynhtruong', ht);
 
--- routes/huynhtruong.js
+++ routes/huynhtruong.js
@@ -1,14 +1,27 @@
 const express= require('express');
 const db= require('../data/db.js');
 const router= express.Router();
+const ht= 'HuynhTruong';
 
 router.get('/', (req, res)=>{
-    db.find('HuynhTruong')
+    db.find(ht)
     .then(ht=>{
         res.status(201).json(ht)
     })
     .catch(error=>{
         res.status(500).json({message: 'fail to fetch list of ht'})
+    })
+})
+router.post('/name', (req, res)=>{
+    const params= req.body
+    console.log(params)
+    db.findByName(ht,params.FullName)
+    .then(ht=>{
+        console.log(ht)
+        res.status(201).json(ht)
+    })
+    .catch(error=>{
+        res.status(500).json({message: `fail to fetch ${params.FullName}'s contact` })
     })
 })
 
