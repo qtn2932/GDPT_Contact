4ccaac9fa101c56a6018cb9e847e2032afaa8510
unknown <quan.nguyen199712@gmail.com>
2018-12-15 14:32:06 -0500

    cmmit with new app

--- client/contacts-app/src/App.js
+++ client/contacts-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import InputForm from './inputForm'
+import InputForm from './inputForm';
+
 
 class App extends Component {
   render() {
